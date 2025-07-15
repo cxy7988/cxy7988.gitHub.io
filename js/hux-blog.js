163b2f9896038062016df397d494a607a03e9f5b
Xinyu Chen <cxy7988@gmail.com>
2025-07-15 11:07:51 +0800

    update

--- js/hux-blog.js
+++ js/hux-blog.js
@@ -6,12 +6,12 @@
 
  /*!
  * Hux Blog v1.6.0 (http://startbootstrap.com)
- * Copyright 2016 @huxpro
+ * Copyright 2025 @cxy7988
  * Licensed under Apache 2.0 
  */
 
 // Tooltip Init
-// Unuse by Hux since V1.6: Titles now display by default so there is no need for tooltip
+// Unuse by Brian since V1.6: Titles now display by default so there is no need for tooltip
 // $(function() {
 //     $("[data-toggle='tooltip']").tooltip();
 // });
@@ -19,7 +19,7 @@
 
 // make all images responsive
 /* 
- * Unuse by Hux
+ * Unuse by Brian
  * actually only Portfolio-Pages can't use it and only post-img need it.
  * so I modify the _layout/post and CSS to make post-img responsive!
  */
